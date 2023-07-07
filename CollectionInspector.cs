c3442ca8f7bc31d558c96160a1f2237635f6b5a8
thines01 <thines01@yahoo.com>
2023-07-06 21:57:04 -0500

    Add project files.

---
+++ CollectionInspector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CollectionInspector
+{
+	public class CCollectionInspector
+	{
+		public static Dictionary<string, int> GetInspectedMap0<T>(IEnumerable<T> arr) where T : class
+		{
+			return
+			(arr.ElementAt(0)).GetType().GetProperties().Select( (pi,i) => 
+				new KeyValuePair<string, int>
+				(
+					pi.Name,
+					arr.Max(obj => obj.GetType().GetProperty(pi.Name)
+						.GetValue(obj, new object[]{i}).ToString().Length)
+				)
+			).ToDictionary(k => k.Key, v => v.Value);
+		}
+
+		public static Dictionary<string, int> GetInspectedMap<T>(IEnumerable<T> arr) where T : class
+		{
+			return
+			(arr.ElementAt(0)).GetType().GetProperties().Select(pi => pi.Name)
+				.Select((strPropertyName) =>
+				new KeyValuePair<string, int>
+				(
+					strPropertyName,
+					arr.Max(obj => obj.GetType().GetProperty(strPropertyName)
+						.GetValue(obj, null).ToString().Length)
+				)
+			).ToDictionary(k => k.Key, v => v.Value);
+		}
+	}
+}
